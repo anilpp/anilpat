d9f241d298bb13e55184a581340738ed65cf6ced
Sabyasachi Mahapatra <Sabyasachi Mahapatra@Milan>
2019-04-26 23:37:37 +0530

    changes in base

--- com.auto/src/main/java/base/baseclass.java
+++ com.auto/src/main/java/base/baseclass.java
@@ -1,5 +1,24 @@
 package base;
 
-public class baseclass {
+import java.util.Set;
 
-}
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class baseclass {
+	
+	public static void main(String[] args){
+
+		
+    System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sabyasachi Mahapatra\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        
+    WebDriver driver=new ChromeDriver();
+      
+ driver.manage().window().maximize();
+ driver.get("https://omnidesk.newgen.co.in/");
+        
+     //   driver.manage.window().deleteAllCookies();
+       // driver.quit();
+		
+		
+}}
